642895cb8c2d8dc6070c4abfc7e21293681f71dc
Myles Lee <mtl115@ic.ac.uk>
2018-12-03 17:12:20 +0000

    redesign gui

--- gui.py
+++ gui.py
@@ -45,6 +45,12 @@ def main():
 	def save_output():
 		pass
 
+	def optimise():
+		pass
+
+	def random_schedule():
+		pass
+
 	def explain():
 		output_text = interface.explain(
 			m_spinbox.get(),
@@ -58,95 +64,100 @@ def main():
 
 		fig.canvas.draw()
 
-	def optimise():
-		pass
-
 	root.protocol("WM_DELETE_WINDOW", quit)
-	root.title('AES')
+	root.title('Argumentative Explainable Scheduler')
 
 	# Create widgets
 	left_frame = Frame(root)
 
-	command_frame = Frame(left_frame)
-	random_problem_button = Button(command_frame, text='Generate random problem', command=random_problem)
-	load_problem_button = Button(command_frame, text='Load problem', command=load_problem)
-	save_problem_button = Button(command_frame, text='Save problem', command=save_problem)
-	save_output_button = Button(command_frame, text='Save output', command=save_output)
-	explain_button = Button(command_frame, text='Explain', command=explain)
-	optimise_button = Button(command_frame,  text='Optimise', command=optimise)
-
-	problem_frame = Frame(left_frame)
-	m_label = Label(problem_frame, text='Number of machines')
+	problem_frame = LabelFrame(left_frame, text='Problem')
+	problem_command_frame = Frame(problem_frame)
+	load_problem_button = Button(problem_command_frame, text='Load', command=load_problem)
+	save_problem_button = Button(problem_command_frame, text='Save', command=save_problem)
+	random_problem_button = Button(problem_command_frame, text='Randomise', command=random_problem)
+	m_label = Label(problem_frame, text='Number of machines', anchor=E)
 	m_spinbox = Spinbox(problem_frame, from_=0, to_=sys.maxsize)
-	p_label = Label(problem_frame, text='Processing times')
+	p_label = Label(problem_frame, text='Processing times', anchor=E)
 	p_textbox = Text(problem_frame, height=3, width=40)
 	p_scrollbar = attach_scrollbar(problem_frame, p_textbox)
-	nfd_label = Label(problem_frame, text='Negative fixed decisions')
+	nfd_label = Label(problem_frame, text='Negative fixed decisions', anchor=E)
 	nfd_textbox = Text(problem_frame, height=3, width=40)
 	nfd_scrollbar = attach_scrollbar(problem_frame, nfd_textbox)
-	pfd_label = Label(problem_frame, text='Positive fixed decisions')
+	pfd_label = Label(problem_frame, text='Positive fixed decisions', anchor=E)
 	pfd_textbox = Text(problem_frame, height=3, width=40)
 	pfd_scrollbar = attach_scrollbar(problem_frame, pfd_textbox)
 
-	S_frame = Frame(left_frame)
-	S_label = Label(S_frame, text='Schedule')
+	S_frame = LabelFrame(left_frame, text='Schedule')
+	S_command_frame = Frame(S_frame)
+	S_optimise_button = Button(S_command_frame, text='Generate optimum', command=optimise)
+	S_random_button = Button(S_command_frame, text='Randomise', command=random_schedule)
 	S_textbox = Text(S_frame, height=3, width=40)
 	S_scrollbar = attach_scrollbar(S_frame, S_textbox)
+	explain_button = Button(S_frame, text='Explain', command=explain)
 
 	right_frame = Frame(root)
 	fig = plt.figure(0)
 	S_figure = FigureCanvasTkAgg(fig, master=right_frame).get_tk_widget()
 	output_textbox = Text(right_frame)
 	output_scrollbar = attach_scrollbar(right_frame, output_textbox)
+	save_output_button = Button(right_frame, text='Save output', command=save_output)
 
 	# Geometry
-	padding = 4
+	padding = 8
 
+	root.geometry("1024x768")
 	root.rowconfigure(0, weight=1)
 	root.columnconfigure(1, weight=1)
 
 	left_frame.grid(row=0, column=0, sticky=N+S)
+	left_frame.rowconfigure(0, weight=1)
 	left_frame.rowconfigure(1, weight=1)
-	left_frame.rowconfigure(2, weight=1)
 
-	command_frame.grid(row=0, column=0, padx=padding, pady=padding)
-	random_problem_button.grid(row=0, column=0, padx=padding, pady=padding)
-	load_problem_button.grid(row=0, column=1, padx=padding, pady=padding)
-	save_problem_button.grid(row=0, column=2, padx=padding, pady=padding)
-	save_output_button.grid(row=1, column=0, padx=padding, pady=padding)
-	explain_button.grid(row=1, column=1, padx=padding, pady=padding)
-	optimise_button.grid(row=1, column=2, padx=padding, pady=padding)
+	problem_frame.grid(row=0, column=0, padx=padding, pady=padding, sticky=N+S)
+	problem_command_frame.grid(row=0, column=0, columnspan=3, padx=padding, pady=padding, sticky=W+E)
 
-	problem_frame.grid(row=1, column=0, padx=padding, pady=padding, sticky=N+S)
-	problem_frame.rowconfigure(1, weight=1)
+	problem_command_frame.columnconfigure(0, weight=1)
+	problem_command_frame.columnconfigure(1, weight=1)
+	problem_command_frame.columnconfigure(2, weight=1)
+	load_problem_button.grid(row=0, column=0, sticky=W+E)
+	save_problem_button.grid(row=0, column=1, padx=padding, sticky=W+E)
+	random_problem_button.grid(row=0, column=2, sticky=W+E)
 	problem_frame.rowconfigure(2, weight=1)
 	problem_frame.rowconfigure(3, weight=1)
-	m_label.grid(row=0, column=0, pady=padding)
-	m_spinbox.grid(row=0, column=1, columnspan=2, pady=padding, sticky=E+W)
-	p_label.grid(row=1, column=0, pady=padding)
-	p_textbox.grid(row=1, column=1, pady=padding, sticky=N+S)
-	p_scrollbar.grid(row=1, column=2, pady=padding, sticky=N+S)
-	nfd_label.grid(row=2, column=0, pady=padding)
-	nfd_textbox.grid(row=2, column=1, pady=padding, sticky=N+S)
-	nfd_scrollbar.grid(row=2, column=2, pady=padding, sticky=N+S)
-	pfd_label.grid(row=3, column=0, pady=padding)
-	pfd_textbox.grid(row=3, column=1, pady=padding, sticky=N+S)
-	pfd_scrollbar.grid(row=3, column=2, pady=padding, sticky=N+S)
+	problem_frame.rowconfigure(4, weight=1)
 
-	S_frame.grid(row=2, column=0, sticky=N+S+W+E)
+	m_label.grid(row=1, column=0, padx=padding, sticky=W+E)
+	m_spinbox.grid(row=1, column=1, columnspan=2, padx=(0, padding), sticky=W+E)
+	p_label.grid(row=2, column=0, padx=padding, pady=padding, sticky=W+E)
+	p_textbox.grid(row=2, column=1, pady=padding, sticky=N+S)
+	p_scrollbar.grid(row=2, column=2, padx=(0, padding), pady=padding, sticky=N+S)
+	nfd_label.grid(row=3, column=0, padx=padding, sticky=W+E)
+	nfd_textbox.grid(row=3, column=1, sticky=N+S)
+	nfd_scrollbar.grid(row=3, column=2, padx=(0, padding), sticky=N+S)
+	pfd_label.grid(row=4, column=0, padx=padding, pady=padding, sticky=W+E)
+	pfd_textbox.grid(row=4, column=1, pady=padding, sticky=N+S)
+	pfd_scrollbar.grid(row=4, column=2, padx=(0, padding), pady=padding, sticky=N+S)
+
+	S_frame.grid(row=1, column=0, padx=padding, pady=padding, ipadx=50, sticky=N+S+W+E)
 	S_frame.rowconfigure(1, weight=1)
 	S_frame.columnconfigure(0, weight=1)
-	S_label.grid(row=0, column=0, columnspan=2)
-	S_textbox.grid(row=1, column=0, sticky=N+S+W+E)
-	S_scrollbar.grid(row=1, column=1, sticky=N+S)
+	S_command_frame.grid(row=0, column=0, columnspan=2, padx=padding, pady=padding, sticky=W+E)
+	S_command_frame.columnconfigure(0, weight=1)
+	S_command_frame.columnconfigure(1, weight=1)
+	S_optimise_button.grid(row=0, column=0, padx=(0, padding), sticky=W+E)
+	S_random_button.grid(row=0, column=1, sticky=W+E)
+	S_textbox.grid(row=1, column=0, padx=(padding, 0), sticky=N+S+W+E)
+	S_scrollbar.grid(row=1, column=1, padx=(0, padding), sticky=N+S)
+	explain_button.grid(row=2, column=0, columnspan=2, padx=padding, pady=padding, sticky=W+E)
 
-	right_frame.grid(row=0, column=1, sticky=N+S+W+E)
+	right_frame.grid(row=0, column=1, padx=padding, pady=padding, sticky=N+S+W+E)
 	right_frame.rowconfigure(0, weight=1)
 	right_frame.rowconfigure(1, weight=1)
 	right_frame.columnconfigure(0, weight=1)
 	S_figure.grid(row=0, column=0, columnspan=2, sticky=N+S+W+E)
 	output_textbox.grid(row=1, column=0, sticky=N+S+W+E)
 	output_scrollbar.grid(row=1, column=1, sticky=N+S)
+	save_output_button.grid(row=2, column=0, columnspan=2, pady=padding, sticky=W+E)
 
 	root.mainloop()
 
