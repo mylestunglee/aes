cc23d7eba9223038723e1556e9b767f3add39e46
Myles Lee <mtl115@ic.ac.uk>
2019-03-01 22:38:34 +0000

    write up main theorem and update plots

--- scripts/plot_profiles.py
+++ scripts/plot_profiles.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import sys
 import csv
 
@@ -7,6 +8,8 @@ full_cpu = []
 full_memory = []
 partial_cpu = []
 partial_memory = []
+naive_cpu = []
+naive_memory = []
 
 for filename in sys.argv[1:]:
 	with open(filename, 'r') as csvfile:
@@ -19,6 +22,9 @@ for filename in sys.argv[1:]:
 			elif row[0] == 'partial':
 				partial_cpu.append(float(row[2]))
 				partial_memory.append(float(row[3]) / 1024)
+			elif row[0] == 'naive':
+				naive_cpu.append(float(row[2]))
+				naive_memory.append(float(row[3]) / 1024)
 
 small_scale = 8
 big_scale = 0.2
@@ -32,18 +38,21 @@ def draw_legend():
 		lh._sizes = [30]
 
 cpu_figure = plt.figure(1)
-plt.scatter(job_samples, full_cpu, label='full precomputation', s=small_scale, alpha=alpha)
-plt.scatter(job_samples, partial_cpu, label='partial precomputation', s=small_scale, alpha=alpha)
+plt.scatter(job_samples, naive_cpu, label='naive', s=small_scale, alpha=alpha, marker='x', color='grey')
+plt.scatter(job_samples, full_cpu, label='full precomputation', s=small_scale, alpha=alpha, marker='x')
+plt.scatter(job_samples, partial_cpu, label='partial precomputation', s=small_scale, alpha=alpha, marker='x')
 plt.xlabel('$n$')
 plt.ylabel('elapsed time (s)')
 plt.xlim([0, 100])
-plt.ylim([0.6, 1])
+plt.ylim([0.3, 0.6])
 draw_legend()
 cpu_figure.savefig('precomputation_cpu_small.png', dpi=300)
 
 cpu_figure = plt.figure(2)
-plt.scatter(job_samples, full_cpu, label='full precomputation', s=big_scale, alpha=alpha)
-plt.scatter(job_samples, partial_cpu, label='partial precomputation', s=big_scale, alpha=alpha)
+plt.gca().add_patch(patches.Rectangle((0, 0.3), 100, 0.3, linewidth=1, linestyle=':', edgecolor='black', facecolor='none'))
+plt.scatter(job_samples, naive_cpu, label='naive', s=big_scale, alpha=alpha, marker='x', color='grey')
+plt.scatter(job_samples, full_cpu, label='full precomputation', s=big_scale, alpha=alpha, marker='x')
+plt.scatter(job_samples, partial_cpu, label='partial precomputation', s=big_scale, alpha=alpha, marker='x')
 plt.xlabel('$n$')
 plt.ylabel('elapsed time (s)')
 plt.xlim([0, 800])
@@ -52,18 +61,21 @@ draw_legend()
 cpu_figure.savefig('precomputation_cpu_big.png', dpi=300)
 
 memory_figure = plt.figure(3)
-plt.scatter(job_samples, full_memory, label='full precomputation', s=small_scale, alpha=alpha)
-plt.scatter(job_samples, partial_memory, label='partial precomputation', s=small_scale, alpha=alpha)
+plt.scatter(job_samples, naive_memory, label='naive', s=small_scale, alpha=alpha, marker='x', color='grey')
+plt.scatter(job_samples, full_memory, label='full precomputation', s=small_scale, alpha=alpha, marker='x')
+plt.scatter(job_samples, partial_memory, label='partial precomputation', s=small_scale, alpha=alpha, marker='x')
 plt.xlabel('$n$')
 plt.ylabel('maximum allocated memory (MiB)')
 plt.xlim([0, 100])
-plt.ylim([70, 80])
+plt.ylim([50, 60])
 draw_legend()
 memory_figure.savefig('precomputation_memory_small.png', dpi=300)
 
 memory_figure = plt.figure(4)
-plt.scatter(job_samples, full_memory, label='full precomputation', s=big_scale, alpha=alpha)
-plt.scatter(job_samples, partial_memory, label='partial precomputation', s=big_scale, alpha=alpha)
+plt.gca().add_patch(patches.Rectangle((0, 50), 100, 10, linewidth=1, linestyle=':', edgecolor='black', facecolor='none'))
+plt.scatter(job_samples, naive_memory, label='naive', s=big_scale, alpha=alpha, marker='x', color='grey')
+plt.scatter(job_samples, full_memory, label='full precomputation', s=big_scale, alpha=alpha, marker='x')
+plt.scatter(job_samples, partial_memory, label='partial precomputation', s=big_scale, alpha=alpha, marker='x')
 plt.xlabel('$n$')
 plt.ylabel('maximum allocated memory (MiB)')
 plt.xlim([0, 800])
